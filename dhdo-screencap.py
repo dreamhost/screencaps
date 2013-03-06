0bd00e205207f891c38768f0e38a934a3c6c8294
Ryan Petrello <lists@ryanpetrello.com>
2013-03-06 16:37:31 -0500

    Don't attempt to upload to DHO when a screenshot is *not* captured.

--- dhdo-screencap.py
+++ dhdo-screencap.py
@@ -7,6 +7,7 @@ Integrates Mac OS X's screenshot utility with DreamObjects for easy sharing.
 from datetime import datetime
 from contextlib import closing
 import os
+import sys
 import subprocess
 import tempfile
 import webbrowser
@@ -24,26 +25,31 @@ with closing(tempfile.NamedTemporaryFile(mode='rb', suffix='.png')) as f:
     # start interactive screen capture
     result = subprocess.call(['screencapture', '-i', f.name])
 
+    if not os.path.exists(f.name):
+        sys.exit(1)
+
+    if os.stat(f.name).st_size == 0:
+        sys.exit(1)
+
     print 'Screenshot captured! Copying to DreamObjects...'
 
-    if os.path.exists(f.name):
-        connection = boto.connect_s3(
-            aws_access_key_id=dhdo_access_key,
-            aws_secret_access_key=dhdo_secret_key,
-            host='objects.dreamhost.com'
-        )
+    connection = boto.connect_s3(
+        aws_access_key_id=dhdo_access_key,
+        aws_secret_access_key=dhdo_secret_key,
+        host='objects.dreamhost.com'
+    )
 
-        bucket = connection.get_bucket(dhdo_screenshots_bucket)
-        key = bucket.new_key(
-            datetime.strftime(datetime.now(), '%m-%d-%Y-%H-%M-%S') + '.png'
-        )
-        key.set_contents_from_filename(f.name)
-        key.set_canned_acl('public-read')
+    bucket = connection.get_bucket(dhdo_screenshots_bucket)
+    key = bucket.new_key(
+        datetime.strftime(datetime.now(), '%m-%d-%Y-%H-%M-%S') + '.png'
+    )
+    key.set_contents_from_filename(f.name)
+    key.set_canned_acl('public-read')
 
-        public_url = key.generate_url(0, query_auth=False, force_http=True)
+    public_url = key.generate_url(0, query_auth=False, force_http=True)
 
-        print 'Screenshot available at:'
-        print '\t', public_url
+    print 'Screenshot available at:'
+    print '\t', public_url
 
-        os.system('echo "%s" | pbcopy' % public_url)
-        webbrowser.open(public_url)
+    os.system('echo "%s" | pbcopy' % public_url)
+    webbrowser.open(public_url)
